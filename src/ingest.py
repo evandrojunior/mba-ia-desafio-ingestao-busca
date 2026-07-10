f91d2845a39002c21c3c31dd1ae8fa021a2b3eee
Evandro Goncalves <efgjunior@gmail.com>
2026-07-10 13:19:33 -0300

    Update requirements and implement PDF ingestion with environment variable checks

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,51 @@
 import os
+
 from dotenv import load_dotenv
 
-load_dotenv()
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
 
-PDF_PATH = os.getenv("PDF_PATH")
+load_dotenv(override=True)
+for k in ("OPENAI_API_KEY", "OPENAI_EMBEDDING_MODEL","DATABASE_URL","PG_VECTOR_COLLECTION_NAME", "PDF_PATH"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
 
 def ingest_pdf():
-    pass
+    docs = PyPDFLoader(str(os.getenv("PDF_PATH"))).load()
 
+    splits = RecursiveCharacterTextSplitter(
+        chunk_size=1000, 
+        chunk_overlap=150, add_start_index=False).split_documents(docs)
+    if not splits:
+        raise SystemExit(0)
+
+    enriched = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in splits
+    ]    
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = OpenAIEmbeddings(
+        model=os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"),
+        check_embedding_ctx_length=False,
+        base_url=os.getenv("OPENAI_API_BASE") or None
+    )
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
 
 if __name__ == "__main__":
     ingest_pdf()
