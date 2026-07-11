f52a1ce334a150d6e7f143ffa8809168011e0365
Evandro Goncalves Junior <efgjunior@gmail.com>
2026-07-10 23:37:10 -0300

    Implement user input for chat question and enhance search_prompt with environment variable checks (#2)

--- src/chat.py
+++ src/chat.py
@@ -1,13 +1,14 @@
 from search import search_prompt
 
 def main():
-    chain = search_prompt()
+    question = input("Faça sua pergunta: ")
+    chain = search_prompt(question)
 
     if not chain:
         print("Não foi possível iniciar o chat. Verifique os erros de inicialização.")
         return
     
-    pass
+    print(f"Resposta: {chain}")
 
 if __name__ == "__main__":
     main()
--- src/search.py
+++ src/search.py
@@ -1,3 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_classic.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_postgres import PGVector
+
 PROMPT_TEMPLATE = """
 CONTEXTO:
 {contexto}
@@ -25,5 +32,43 @@ PERGUNTA DO USUÁRIO:
 RESPONDA A "PERGUNTA DO USUÁRIO"
 """
 
+load_dotenv(override=True)
+for k in ("OPENAI_EMBEDDING_MODEL","DATABASE_URL","PG_VECTOR_COLLECTION_NAME"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
 def search_prompt(question=None):
-    pass
+    if question is None:
+        return
+    
+    embeddings = OpenAIEmbeddings(
+        model=os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-small"),
+        check_embedding_ctx_length=False,
+        base_url=os.getenv("OPENAI_API_BASE") or None
+    )
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    docs = store.similarity_search_with_score(question, k=10)
+    context = "\n".join([doc.page_content for doc, score in docs])
+
+    template = PromptTemplate(
+        input_variables=["contexto", "pergunta"],
+        template=PROMPT_TEMPLATE
+    )
+
+    formatted_prompt = template.format(contexto=context, pergunta=question)
+
+    chat_model = ChatOpenAI(
+        model_name=os.getenv("OPENAI_MODEL", "gpt-5-nano"),
+        temperature=0.7,
+        base_url=os.getenv("OPENAI_API_BASE") or None
+    )
+    response = chat_model.invoke(formatted_prompt).content
+
+    return response
